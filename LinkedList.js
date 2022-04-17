40cea76cda802b84540399569c9b5ec7f3af762d
Julliet Nyaware <julienyaware@gmail.com>
2022-04-17 03:08:04 +0300

    Added a node which is like the atomic unit. It contains data and a link to the next node.

---
+++ LinkedList.js
@@ -0,0 +1,7 @@
+import Node from "./Node";
+
+class LinkedList {
+    constructor(){
+        this.head = null
+    }
+}
---
+++ Node.js
@@ -0,0 +1,29 @@
+// This is a fundamental building block to many data structures.
+// It mostly contains data and a link to the next node 
+
+
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next - null
+    }
+
+    setNextNode(node) {
+        // Check if whatever is passed as a node is actually a node
+        if (!(node instanceof Node)) {
+            throw new Error('Please ensure next node is an instance of node class')
+        }
+
+        this.next = node
+    }
+
+    setNext(data) {
+        this.next = data
+    }
+
+    getNextNode() {
+        return this.next;
+    }
+}
+
+module.exports = Node
