b70705aba9b5c93ac2b8572f785e5f08b1756159
Julliet Nyaware <julienyaware@gmail.com>
2022-06-30 14:56:11 +0300

    Added linked list implementation.

--- LinkedList.js
+++ LinkedList.js
@@ -1,7 +1,29 @@
-import Node from "./Node";
-
-class LinkedList {
-    constructor(){
-        this.head = null
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
+}
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+
+
+addItem(value){
+  let newNode = new Node();
+  if(!this.head){
+      this.head = newNode;
+      this.length ++ ;
+  } else {
+      let currentNode = this.head;
+      while(currentNode && currentNode.next){
+          currentNode = currentNode.next;
+      }
+      currentNode.next = newNode
+  }
+}
+
+
 }
--- Node.js
+++ Node.js
@@ -5,7 +5,7 @@
 class Node {
     constructor(data) {
         this.data = data
-        this.next - null
+        this.next = null
     }
 
     setNextNode(node) {
