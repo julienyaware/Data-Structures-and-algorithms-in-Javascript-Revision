bc19b42fea3657319513fe90e24a798e1648f00b
Julliet Nyaware <julienyaware@gmail.com>
2022-07-04 13:24:01 +0300

    Implemented a naive hash table with likelihood of collision

--- HashMap.js
+++ HashMap.js
@@ -1,23 +1,42 @@
 // Hashmap example with hash key that may likely cause collission
 
-class NaiveHashMap {
-    constructor (initialValue = 2){
-    this.bucketList = new Array(initialValie)
+class NaiveHashTable {
+    constructor() {
+        this.table = new Array(137)
     }
-    
-    get(key) {
-    let value = this.bucketList[key] ;
-    return value ;
+
+    simpleHash(data) {
+        let position = 0;
+
+        for (let i = 0; i < data.length; i++) {
+            position += data.charCodeAt(i);
+        }
+        return position % this.table.length;;
     }
-    
-    set(key,value){
-    let indexOfHashMap =  getKey(key)
-    this.bucketList[indexOfHashMap] =  value ;
+
+
+    put(value) {
+        let position = this.simpleHash(value);
+        this.table[position] = value;
     }
-    
-    getKey(key) {
-    let keyString = key.toString();
-    
-    
+
+    showDistribution() {
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i] != undefined) {
+                console.log(i + ': ' + this.table[i])
+            }
+        }
+
     }
-    }
+
+}
+
+const students = ["David", "Jennifer", "Donnie", "Raymond",
+    "Cynthia", "Mike", "Clayton", "Danny", "Jonathan"];
+const hTable = new NaiveHashTable();
+for (let i = 0; i < students.length; i++) {
+    hTable.put(students[i]);
+}
+
+
+hTable.showDistribution();
