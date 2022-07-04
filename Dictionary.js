45b8b756d85152b2bddfcfa653d474b6f6b2a295
Julliet Nyaware <julienyaware@gmail.com>
2022-07-04 11:40:47 +0300

    implemented dictionary

--- Dictionary.js
+++ Dictionary.js
@@ -1,51 +1,50 @@
 // Implementation of a simple dictionary
 
-class Dictionary{
-    constructor(){
-    this.dataStore = new Array () ;
+class Dictionary {
+    constructor() {
+        this.dataStore = new Array();
     }
-    
-    addItem(key,value) {
-    this.dataStore[key] = value ;
+
+    addItem(key, value) {
+        this.dataStore[key] = value;
     }
-    
+
     findItem(key) {
-    return this.dataStore[key] ;
+        return this.dataStore[key];
     }
-    
+
     removeItem(key) {
-    delete this.dataStore[key] ; 
+        delete this.dataStore[key];
     }
-    
-    
-    print(){
-        for(const key of Object.keys(this.dataStore)){
-            console.log(key + ' ' +  this.dataStore[key])
-            }
+
+
+    print() {
+        for (const key of Object.keys(this.dataStore)) {
+            console.log(key + ' ' + this.dataStore[key])
+        }
     }
 
     // returns the size of dictionary
-    count(){
-    let n = 0
-    for(const key of Object.keys(this.dataStore)){
-        n += 1
-    }
-    return n ;
+    count() {
+        let n = 0
+        for (const key of Object.keys(this.dataStore)) {
+            n += 1
+        }
+        return n;
     }
 
     // Clear entire dictionary
     clearAll() {
-        for(const key of Object.keys(this.dataStore)){
-            delete this.dataStore[key] ;
-        } 
+        for (const key of Object.keys(this.dataStore)) {
+            delete this.dataStore[key];
+        }
     }
-    
-    }
-    
-    const phoneBook = new Dictionary();
-    phoneBook.addItem('Julliet','0713982943')
-    phoneBook.addItem('Mum','0713982943')
-    phoneBook.addItem('Charles','0713982943')
-    
-     phoneBook.print();
-     
+
+}
+
+const phoneBook = new Dictionary();
+phoneBook.addItem('Julliet', '0713988843')
+phoneBook.addItem('Mum', '07139825543')
+phoneBook.addItem('Charles', '0713552943')
+
+phoneBook.print();
